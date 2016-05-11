26d5072ef4b87b8caafda4429a8bf76bcce962cc
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-11 18:21:41 +0530

    Initial commit
    Path: config/tasks/update.js
    Why? – To automate the updating of our "package.json" packages with a grunt task.
    Usage: $ npm install grunt-dev-update --save-dev

---
+++ config/tasks/update.js
@@ -0,0 +1,22 @@
+/*!
+ * BioQL®: An efficient Query Language for interpreting genome and proteome.
+ * ___________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~
+ * Update           : @require ============================================
+ *   |-------------- Update devDependencies and dependencies automatically.
+ * grunt-dev-update : https://github.com/pgilad/grunt-dev-update
+ * Usage            : $ npm install grunt-dev-update --save-dev
+ * ___________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ___________________________________________________________________________
+ * @date      : 11-May-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ___________________________________________________________________________
+ */
