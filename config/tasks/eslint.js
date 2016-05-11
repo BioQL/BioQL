7769eef942ef5bf9737ac00392698c3bccd2787c
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-11 18:15:22 +0530

    Initial commit
    Path: config/tasks/eslint.js
    ESLint - A fully pluggable tool for identifying and reporting on patterns in JavaScript.
    Usage: $ npm install eslint -g

---
+++ config/tasks/eslint.js
@@ -0,0 +1,22 @@
+/*!
+ * BioQL®: An efficient Query Language for interpreting genome and proteome.
+ * ___________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~
+ * ESLint     : https://github.com/eslint/eslint
+ * Help       : http://eslint.org/
+ * Usage      : $ npm install eslint -g
+ * @require   : rules/.eslintrc
+ * ___________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ___________________________________________________________________________
+ * @date      : 11-May-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ___________________________________________________________________________
+ */
