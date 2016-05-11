1ac86a8eda2f0e1a1a1d12bd422bb18d70222a38
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-11 14:30:59 +0530

    Initial commit
    --/The Heart of Build System/-- of "BioQL®".
    Gruntfile.js, configuring the JavaScript Task Runner.
    Help: Help: https://github.com/gruntjs or http://gruntjs.com/ and http://gruntjs.com/configuring-tasks
    Usage: $ grunt build

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+
+/*!
+ * BioQL®: An efficient Query Language for interpreting genome and proteome.
+ * @version   : [1.0.0]
+ * ___________________________________________________________________________
+ * CBBL®
+ * ~~~~~
+ * Computational Biology and Bioinformatics Laboratory
+ * Υπολογιστικής Βιολογίας και Βιοπληροφορικής Εργαστήριο
+ * ___________________________________________________________________________
+ * Grunt, http://gruntjs.com/
+ * The JavaScript Task Runner.
+ * ___________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ___________________________________________________________________________
+ * @date      : 11-May-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Build System/-- of "BioQL®".
+ * ___________________________________________________________________________
+ */
