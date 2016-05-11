d53c6e88a96e81abf73817f338e39f57370957eb
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-11 14:34:51 +0530

    Initial commit
    --/The Heart of Test System/-- of "BioQL®".
    karma, The Spectacular Test Runner for JavaScript.
    Help: https://github.com/karma-runner/karma
    Usage: $ karma init

---
+++ karma.conf.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+
+/*!
+ * BioQL®: An efficient Query Language for interpreting genome and proteome.
+ * @version   : [1.0.0]
+ * ___________________________________________________________________________
+ * CBBL®
+ * ~~~~~
+ * Computational Biology and Bioinformatics Laboratory
+ * Υπολογιστικής Βιολογίας και Βιοπληροφορικής Εργαστήριο
+ * ___________________________________________________________________________
+ * karma, https://github.com/karma-runner/karma
+ * The Spectacular Test Runner for JavaScript.
+ * ___________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ___________________________________________________________________________
+ * @date      : 11-May-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ___________________________________________________________________________
+ *
+ * --/The Heart of Karma Test Runner System/-- of "BioQL®".
+ * ___________________________________________________________________________
+ */
