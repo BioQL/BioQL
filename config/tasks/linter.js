a9675c617f82b817eb4482be8da86bf9e0867f35
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-05-11 18:18:10 +0530

    Initial commit
    Path: config/tasks/linter.js
    Usage: $ npm install grunt-contrib-jshint --save-dev

---
+++ config/tasks/linter.js
@@ -0,0 +1,22 @@
+/*!
+ * BioQL®: An efficient Query Language for interpreting genome and proteome.
+ * ___________________________________________________________________________
+ * Grunt, The JavaScript Task Runner - http://gruntjs.com/.
+ * ~~~~~~~~~
+ * Linter(s)              : @require =========================================
+ * grunt-contrib-jshint   : https://github.com/gruntjs/grunt-contrib-jshint
+ * Usage              	  : $ npm install grunt-contrib-jshint --save-dev
+ * @require               : rules/.jshintrc
+ * ___________________________________________________________________________
+ * Architecture and Code Handcrafted by Prabhat Kumar.
+ * Architectuur en Code handgemaakt door Prabhat Kumar.
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Sequømics Corporation [http://sequomics.com/]
+ * ___________________________________________________________________________
+ * @date      : 11-May-2016
+ * @license   : Apache, version 2.0
+ * @require   : Node.js®
+ * @require   : NPM
+ * @require   : Grunt
+ * ___________________________________________________________________________
+ */
