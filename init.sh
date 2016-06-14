bfabed4764f12387772bcd3a79e4fdb5b1d9e3b2
Computational Biology & Bioinformatics Laboratory <cb2labs@gmail.com>
2016-06-14 13:48:09 +0530

    Initial commit
    init.sh, shell script to --/initialize/-- whole "BioQL®" in one go.
    Usage: $ sh init.sh

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+# ============================== BioQL® ==============================
+#   An efficient Query Language for interpreting genome and proteome.
+#                                CBBL®
+#           Computational Biology and Bioinformatics Laboratory
+#         Υπολογιστικής Βιολογίας και Βιοπληροφορικής Εργαστήριο
+#         Handcrafted by Prabhat Kumar, http://prabhatkumar.org/.
+# Copyright © 2014 - 2016, Sequømics Corporation. All rights reserved.
+# ====================================================================
